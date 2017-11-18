68bf376122577bab20b7f59ee92013d686ce74f3
keweyt <heliu0926@163.com>
2017-11-18 17:12:43 +0800

    add js file

---
+++ dealMove.js
@@ -0,0 +1,17 @@
+'use strict';
+function get_str(num,len){
+	return parseInt(num).toString(i);
+
+}
+function all_num(m,n){
+	return Math.pow(m,n);
+}
+
+var mm=all_num(3,6);
+console.log(mm);
+var allstrs=[];
+for(var i=0;i<mm;i++){
+	var str=get_str(i,3);
+	console.log(str);
+	allstrs.push(str);
+}
